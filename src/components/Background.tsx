4df8ab95e8ae410ec013e75a6bec1b5922f354d1
hemantch01 <hemantchaudhary905@gmail.com>
2025-07-26 12:40:10 +0000

    added falling physic

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,6 +1,6 @@
 import img from "../assets/background-day.png"
 export const Background = ()=>{
-    return <div className="relative flex justify-center w-full h-screen">
+    return <div className=" my-4 flex justify-center w-full h-[800px]">
        <img src={img} alt="Background" />
     </div>
 }
--- src/components/FlappyBird.tsx
+++ src/components/FlappyBird.tsx
@@ -1,6 +1,30 @@
-import img from "../assets/yellowbird-upflap.png"
-export const FlappyBird = ()=>{
-    return <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-       <img src={img} className="w-18 h-12"/>
+import { useEffect, useState } from "react";
+import img from "../assets/yellowbird-upflap.png";
+
+export const FlappyBird = () => {
+  const [BirdPos, setBirdPos] = useState(300);
+
+  useEffect(() => {
+    const groundLevel = 780;  
+    const interval = setInterval(() => {
+      setBirdPos((pos) => {
+        if (pos < groundLevel) {
+          return pos + 10;
+        } else {
+            return groundLevel;
+        }
+      });
+    }, 15); // Adjust speed as needed
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div
+      className="absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+      style={{ top: `${BirdPos}px` }}
+    >
+      <img src={img} className="w-18 h-12" alt="bird" />
     </div>
-}
+  );
+};
